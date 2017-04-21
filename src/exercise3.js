40f252816e7ff3af2db21c76b7dc33d0a9836d74
zack <remake35@gmail.com>
2017-04-21 10:01:35 +0200

    git commit -m "Exercise 3"

--- src/exercise3.js
+++ src/exercise3.js
@@ -3,8 +3,10 @@ const chalk = require('chalk');
 console.log(chalk.cyan('\nEXERCISE 3 LOADED\n'));
 
 const reverseString = (input) => {
-
-  // Your future job begins here ...
+  function reverse(s, acc) {
+    return (s.length == 0) ? acc : reverse(s.substr(1), s.charAt(0) + acc);
+  };
+  return reverse(input, '');
 }
 
 module.exports = {
