a7a37ae4d1bad391996f364dfec10c8ddcbd8646
zack <remake35@gmail.com>
2017-04-21 09:30:48 +0200

    git commit -m "Exercise 1"

--- src/exercise1.js
+++ src/exercise1.js
@@ -2,9 +2,16 @@ const chalk = require('chalk');
 
 console.log(chalk.cyan('\nEXERCISE 1 LOADED\n'));
 
-const zip = (input) => {
-
-  // Your future job begins here ...
+const zip = (arrays) => {
+	return arrays.reduce(function (acc, arr, i) {
+	        while (acc.length < arr.length) {
+	            acc.push([]);
+	        }
+	        for (var j = 0; j < arr.length; ++j) {
+	            acc[j][i] = arr[j];
+	        }
+	        return acc;
+	    }, []);
 }
 
 module.exports = {
